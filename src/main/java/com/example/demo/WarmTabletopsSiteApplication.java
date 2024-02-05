354cc76210daad3e260844caf437d1dd546f5f87
Denis <denis.petrov56245@gmail.com>
2024-02-05 23:58:59 +0100

    first commit

---
+++ src/main/java/com/example/demo/WarmTabletopsSiteApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarmTabletopsSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarmTabletopsSiteApplication.class, args);
+	}
+
+}
