334918eef8af32128dc38e9639844b1c33afdbea
Denis <denis.petrov56245@gmail.com>
2024-02-06 14:52:30 +0100

    new branch

--- src/main/java/com/example/demo/Controller/MainController.java
+++ src/main/java/com/example/demo/Controller/MainController.java
@@ -1,7 +1,9 @@
 package com.example.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
@@ -9,5 +11,20 @@ public class MainController {
     @RequestMapping("/")
     public String homePage() {
         return "mainsite";
+    }
+
+    @RequestMapping("/games-catalog")
+    public String gamesCatalog() {
+        return "gamesCatalog";
+    }
+
+    @RequestMapping("/hogwarts-site")
+    public String hogwartsSite() {
+        return "hogwartsSite";
+    }
+
+    @RequestMapping("/arkandia-site")
+    public String arkandiaSite() {
+        return "arkandiaSite";
     }
 }
