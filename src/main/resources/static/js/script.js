ed2c62f01e26074e148fe1f0a9fcb942d2d3d5d8
Denis <denis.petrov56245@gmail.com>
2024-04-09 11:07:20 +0200

    test new site

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,10 @@
+
+    function toggleSidebar() {
+        var sidebar = document.getElementById("sidebar");
+        if (sidebar.style.display === "block") {
+            sidebar.style.display = "none";
+        } else {
+            sidebar.style.display = "block";
+        }
+    }
+
